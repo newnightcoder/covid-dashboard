9db8e0a99618ce89e7bf4e9ce54e7e235a31bef7
Nightcoder <newnightcoder@gmail.com>
2021-02-16 22:05:55 +0100

    react covid dashboard :rocket:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  AppHeader,
+  Counters,
+  CountryTable,
+  Search,
+  More,
+  // GlobeSection,
+} from "./components";
+import { fetchBrief, fetchCountries } from "./api";
+// import axios from "axios";
+// import Gio from "react-giojs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: {},
+    countries: [],
+    showMore: false,
+    btnText: "more",
+    geoData: null,
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchBrief();
+    this.setState({ data: fetchedData });
+
+    const fetchedCountries = await fetchCountries();
+    this.setState({ countries: fetchedCountries });
+
+    // axios
+    //   .get(`../data/sampleData.json`)
+    //   .then((response) => this.setState({ geoData: response.data }));
+  }
+
+  toggleMore = () => {
+    this.setState({ showMore: !this.state.showMore });
+    if (this.state.showMore) {
+      this.setState({ btnText: "more" });
+    } else this.setState({ btnText: "close" });
+  };
+
+  more = () => {
+    if (this.state.showMore) {
+      return <More data={this.state.data} />;
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        {" "}
+        <AppHeader data={this.state.data} />
+        <Counters
+          btnText={this.state.btnText}
+          toggleMore={this.toggleMore}
+          data={this.state.data}
+        />
+        {this.more()}
+        {/* <GlobeSection /> */}
+        <Search />
+        <CountryTable data={this.state.countries} />
+      </div>
+    );
+  }
 }
 
 export default App;
